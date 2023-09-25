308af23611614429c8105a06202239b8aaf02013
EdCarlos-dev <bio_edc@hotmail.com>
2023-09-25 20:39:12 -0300

    Revert "update 2.0"
    This reverts commit fd2a863f3eba4dd76542454ce3836e026278c7b8.

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const clinicaController = require('./controllers/clinicaController');
-
-router.get('/clinica', clinicaController.buscarPacientes);
-
-
-module.exports = router;
