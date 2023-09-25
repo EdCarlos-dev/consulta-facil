da5f6465ad30de4c39798087a601c9453f9953fb
EdCarlos-dev <bio_edc@hotmail.com>
2023-09-25 20:39:42 -0300

    Revert "update 1.9"
    This reverts commit 8d87470c3f479bfe50eb34b4efac2634cff84611.

---
+++ public/banco.js
@@ -0,0 +1,41 @@
+const mysql = require('mysql');
+const dbConfig = require('./dbconfig'); // Importe as configurações do banco de dados
+
+class Banco {
+  
+  constructor() {
+    this.connection = mysql.createConnection(dbConfig);
+  }
+
+  connect() {
+    this.connection.connect(function (err) {
+      if (err) throw err;
+      console.log('Connected to the database!');
+    });
+  }
+
+  insertPaciente(nome, email, senha, convenio, sus, callback) {
+    const sql = `INSERT INTO pacientes (nome, email, senha, convenio, sus) VALUES (?, ?, ?, ?, ?)`;
+    const values = [nome, email, senha, convenio, sus];
+
+    this.connection.query(sql, values, function (err, result) {
+      if (err) {
+        console.error('Erro ao inserir paciente:', err);
+        callback(err);
+      } else {
+        console.log('1 record inserted');
+        callback(null, result);
+      }
+    });
+  }
+
+  closeConnection() {
+    this.connection.end(function (err) {
+      if (err) throw err;
+      console.log('Connection closed.');
+    });
+  }
+  var connection;
+}
+
+module.exports = new Banco(); // Exporte uma instância única do Banco
