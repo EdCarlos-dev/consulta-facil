b7374db6bff5122885d73a2e97ec23e88c67c81e
EdCarlos-dev <bio_edc@hotmail.com>
2023-10-15 19:35:46 -0300

    Cadastro de paciente , funcionando

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const path = require('path');
 const { Sequelize, DataTypes } = require('sequelize');
+
 const app = express();
 const port = 3000;
 
 // Configure a conexão com o banco de dados
 const sequelize = new Sequelize({
-  dialect: 'mysql', // Substitua pelo seu banco de dados (por exemplo, postgres, sqlite, etc.)
-  host: 'localhost', // Substitua pelo host do seu banco de dados
+  dialect: 'mysql',
+  host: 'localhost',
   username: 'root',
-  password: '123456',
+  password: '23581321',
   database: 'clinica',
 });
 
@@ -36,6 +37,9 @@ const User = sequelize.define('User', {
     type: DataTypes.INTEGER,
     allowNull: false,
   },
+
+},{
+  tableName: 'users',
 });
 
 // Sincronize o modelo com o banco de dados e aplique quaisquer alterações necessárias
@@ -49,98 +53,112 @@ sequelize.sync({ alter: true })
 
 // Middleware para processar dados de formulário
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Middleware para servir arquivos estáticos, incluindo o CSS
 app.use(express.static(path.join(__dirname, '/public')));
 
-// Configure o Express para servir arquivos estáticos na pasta "public"
-
-app.use(express.static('public'));
-
 // Rota para a página inicial
-app.get('/', function(req, res) {
-  // Você pode enviar um arquivo HTML como resposta ou renderizar uma página, dependendo da estrutura do seu projeto
-  res.sendFile(__dirname + "/public/index.html");
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-// Rota para a página cadastro
-app.get('/cadastro', function(req, res) {
-  // Você pode enviar um arquivo HTML como resposta ou renderizar uma página, dependendo da estrutura do seu projeto
-  res.sendFile(__dirname + "/public/cadastro.html");
+// Rota para a página de cadastro
+app.get('/cadastro', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/cadastro.html'));
 });
 
-// Rota para a página cadastro
-app.get('/cadastro-medico', function(req, res) {
-  // Você pode enviar um arquivo HTML como resposta ou renderizar uma página, dependendo da estrutura do seu projeto
-  res.sendFile(__dirname + "/public/cadastromedico.html");
+// Rota para a página de cadastro médico
+app.get('/cadastro-medico', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/cadastromedico.html'));
 });
 
-// Rota para a página login
-app.get('/login', function(req, res) {
-  // Você pode enviar um arquivo HTML como resposta ou renderizar uma página, dependendo da estrutura do seu projeto
-  res.sendFile(__dirname + "/public/login.html");
+// Rota para a página de login
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/login.html'));
 });
 
-// Rota para a página esqueci senha
-app.get('/recuperar-senha', function(req, res) {
-  // Você pode enviar um arquivo HTML como resposta ou renderizar uma página, dependendo da estrutura do seu projeto
-  res.sendFile(__dirname + "/public/recuperar-senha.html");
+// Rota para a página de recuperação de senha
+app.get('/recuperar-senha', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/recuperar-senha.html'));
 });
 
-// Rota para a página busca
-app.get('/busca', function(req, res) {
-  // Você pode enviar um arquivo HTML como resposta ou renderizar uma página, dependendo da estrutura do seu projeto
-  res.sendFile(__dirname + "/public/busca.html");
+// Rota para a página de busca
+app.get('/busca', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/busca.html'));
 });
 
-// Rota para a página medicamentos
-app.get('/medicamentos', function(req, res) {
-  // Você pode enviar um arquivo HTML como resposta ou renderizar uma página, dependendo da estrutura do seu projeto
-  res.sendFile(__dirname + "/public/medicamentos.html");
+// Rota para a página de medicamentos
+app.get('/medicamentos', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/medicamentos.html'));
 });
 
-// Rota para a página agendamentos
-app.get('/agendamentos', function(req, res) {
-  // Você pode enviar um arquivo HTML como resposta ou renderizar uma página, dependendo da estrutura do seu projeto
-  res.sendFile(__dirname + "/public/agendamentos.html");
+// Rota para a página de agendamentos
+app.get('/agendamentos', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/agendamentos.html'));
 });
 
-// Rota para a Marcar consulta
-app.get('/marcar-consulta', function(req, res) {
-  // Você pode enviar um arquivo HTML como resposta ou renderizar uma página, dependendo da estrutura do seu projeto
-  res.sendFile(__dirname + "/public/marcarconsulta.html");
+// Rota para a página de marcar consulta
+app.get('/marcar-consulta', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/marcarconsulta.html'));
 });
 
-// Rota para a Perfil Paciente
-app.get('/perfilPaciente', function(req, res) {
-  // Você pode enviar um arquivo HTML como resposta ou renderizar uma página, dependendo da estrutura do seu projeto
-  res.sendFile(__dirname + "/public/perfilPaciente.html");
+// Rota para a página de perfil do paciente
+app.get('/perfilPaciente', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/perfilPaciente.html'));
 });
 
-// Rota para requisição de novo login
+// Rota para a requisição de novo cadastro
+app.post("/cadastrar", async (req, res) => {
+  try {
+    const { nome, email, senha, convenio, sus, concordouCheckbox } = req.body;
+
+    // Faça as validações necessárias antes de criar o usuário
+
+    // Crie o usuário com os dados extraídos
+    const novoUsuario = await User.create({
+      nome,
+      email,
+      senha,
+      convenio,
+      sus,
+      concordou: concordouCheckbox === 'on', // checkbox é enviado apenas se marcado
+    });
+
+    return res.json({
+      erro: false,
+      mensagem: "Cadastro efetuado com sucesso",
+      usuario: novoUsuario,
+    });
+  } catch (error) {
+    console.error("Erro ao cadastrar usuário:", error);
+
+    // Mantenha mensagens de erro consistentes
+    return res.status(400).json({
+      erro: true,
+      mensagem: "Erro ao cadastrar usuário. Verifique os dados e tente novamente.",
+    });
+  }
+});
+
+// Rota para a requisição de novo login
 app.post("/login", async (req, res) => {
-
   console.log(req.body);
 
-  await User.create(req.body)
-  .then(() => {
+  try {
+    await User.create(req.body);
     return res.json({
       erro: false,
       mensagem: "Login efetuado com sucesso"
-    })
-
-  }).catch(() => {
+    });
+  } catch (error) {
+    console.error("Erro ao criar usuário:", error);
     return res.status(400).json({
       erro: true,
       mensagem: "Login inválido. Verifique os dados e tente novamente."
     });
-  });
-
-  const { email, senha } = req.body;
-
+  }
 });
-
-// Outras rotas e configurações...
 
 // Inicie o servidor
 app.listen(port, () => {
