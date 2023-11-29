6e5e6ec4277092518f1611dcf496705f291b0516
Lipe078 <marketing@letscode.com.br>
2023-11-29 10:54:34 -0300

    update 2..5 prontuarioMedico

--- front/public/prontuarioMedico.js
+++ front/public/prontuarioMedico.js
@@ -1,53 +1,54 @@
 document.addEventListener("DOMContentLoaded", async function () {
-    const pacienteId = 'all'; // Função a ser implementada para obter o ID do paciente da URL
-  
-    // Obter informações do paciente e histórico de consultas
-    //const response = await fetch(`/prontuario-medico/${pacienteId}`, {
-    const response = await fetch(`/consultas-agendadas`, {
-      method: 'GET',
-      headers: {
-        'Authorization': 'Bearer ' + localStorage.getItem('token'),
-      },
-    });
-  
-    const data = await response.json();
-  
-    // Exibir as informações na página
-    exibirInformacoesPaciente(data);
-    exibirHistoricoConsultas(data);
-    console.log(data);
-    exibirComentariosEnfermeiro(data);
+  const pacienteId = 'all';
+
+  // Obter informações do paciente e histórico de consultas
+  const response = await fetch(`/consultas-agendadas`, {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + localStorage.getItem('token'),
+    },
   });
-  
-  function 
-  obterPacienteIdDaURL() {
-    // Implemente a lógica para obter o ID do paciente da URL
+
+  const data = await response.json();
+
+  // Exibir as informações na página
+  exibirInformacoesPaciente(data);
+  exibirHistoricoConsultas(data.historico);
+  console.log(data);
+  exibirComentariosEnfermeiro(data);
+});
+
+function obterPacienteIdDaURL() {
+  // Implemente a lógica para obter o ID do paciente da URL
+  // Retornaremos 'all' por enquanto, você precisará ajustar isso conforme sua lógica
+  return 'all';
+}
+
+function formatarData(data) {
+  const options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+  return new Date(data).toLocaleDateString('pt-BR', options);
+}
+
+function exibirInformacoesPaciente(data) {
+  const tabelaPaciente = document.getElementById('corpo-tabela-paciente');
+
+  if (data && data.length > 0) {
+    const rows = data.map(consulta => {
+      const { paciente_id, nome, data_consulta, especialidade, comentarios, status } = consulta;
+      return `<tr>
+                <td>${paciente_id}</td>
+                <td>${nome}</td>
+                <td>${formatarData(data_consulta)}</td>
+                <td>${especialidade}</td>
+                <td>${comentarios}</td>
+                <td>${status}</td>
+              </tr>`;
+    }).join('');
+    tabelaPaciente.innerHTML = rows;
+  } else {
+    tabelaPaciente.innerHTML = '<tr><td colspan="6">Nenhuma informação do paciente disponível.</td></tr>';
   }
-  
-  function exibirInformacoesPaciente(paciente) {
-    const tabelaPaciente = document.getElementById('corpo-tabela-paciente');
-    
-    if (paciente && paciente.length > 0) {
-      const rows = paciente.map(consulta => {
-        const { paciente_id, nome, data_consulta, especialidade, comentarios, status } = consulta;
-        return `<tr>
-                  <td>${paciente_id}</td>
-                  <td>${nome}</td>
-                  <td>${data_consulta}</td>
-                  <td>${especialidade}</td>
-                  <td>${comentarios}</td>
-                  <td>${status}</td>
-                </tr>`;
-      }).join('');
-      tabelaPaciente.innerHTML = rows;
-    } else {
-      tabelaPaciente.innerHTML = '<tr><td colspan="4">Nenhuma informação do paciente disponível.</td></tr>';
-    }
-  }
-  
-  function exibirHistoricoConsultas(historico) {
-    // Implemente a lógica para exibir o histórico de consultas na página
-  }
+}
   
 // function exibirComentariosEnfermeiro(comentariosEnfermeiro) {
 //     console.log(comentariosEnfermeiro);
